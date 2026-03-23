33a63502876f677e7251ac7e65ee25193bf68f84
SPraveeen <praveenji8498@gmail.com>
2026-03-23 08:00:07 +0530

    Mete data driven pipeline

--- aws_dbt_snowflake_project/models/silver/silver_listings.sql
+++ aws_dbt_snowflake_project/models/silver/silver_listings.sql
@@ -1,12 +1,16 @@
-{{
-    config
-(
-    materialized='incremental',
-    unique_key='listing_id'
-)
-}}
+{{config(materialized='incremental',unique_key='listing_id')}}
 
 select 
-listing_id,listing_name,property_type,room_type,city,country,accomodates,bedrooms,bathrooms,price_per_night,
-{{tag ('cast(price_per_night as int)') }}as price_per_night_tag,created_at
-from ref {{ ref('bronze_listings') }}
+listing_id,
+host_id,
+property_type,
+room_type,
+city,
+country,
+ACCOMMODATES,
+bedrooms,
+bathrooms,
+price_per_night,
+{{tag ('cast(price_per_night as int)') }}as price_per_night_tag,
+created_at
+from {{ ref('bronze_listings') }}
